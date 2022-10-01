2a56228a128c6ba47b0fdbd9cc6a9cff9266cc6b
LaraKnife <192t0220@itsm.edu.mx>
2022-10-01 09:27:35 -0500

    git commit

--- app/src/main/java/com/example/aplicacion01oct/MainActivity.kt
+++ app/src/main/java/com/example/aplicacion01oct/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.aplicacion01oct
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.aplicacion01oct.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.calcular.setOnClickListener {
+            val edadString = binding.edad.text.toString()
+            if (edadString.isEmpty()){
+                Toast.makeText(this,"Ingrese una edad",Toast.LENGTH_LONG).show()
+            }else{
+                val age = edadString.toInt()
+                val edaddog = age * 7
+                binding.resultado.text = "Su edad es años perro sería $edaddog"
+            }
+        }
+
     }
 }
